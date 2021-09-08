be243c724ae13ea88bf1ed0a7ab3ab57f6a955a8
MCLEANS <jackagwaya@gmail.com>
2021-09-08 22:29:36 +0300

    Added task notes

--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
 #include "stm32f4xx.h"
 #include "clockconfig.h"
 
+/**
+ * TASKS
+ * 1. Implement HTTP class
+ * 2. Inherit from the USART class
+ * 3. Initialize USART and sent http packets
+ */
+
 custom_libraries::clock_config system_clock;
 
 int main(void) {
   
   system_clock.initialize();
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-  
-  GPIOA->MODER |= GPIO_MODER_MODER7_0;
-  GPIOA->MODER |= GPIO_MODER_MODER6_0;
-
-  GPIOA->ODR |= GPIO_ODR_ODR_6;
-  GPIOA->ODR &= ~GPIO_ODR_ODR_7;  
 
   while(1){
-    for(volatile int i = 0; i < 2000000; i++){}
-    GPIOA->ODR ^= (1<<6);
-    GPIOA->ODR ^= (1<<7);
 
   }
 }
